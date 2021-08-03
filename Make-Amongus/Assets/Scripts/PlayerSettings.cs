98038e1ee2be2f8ab3aa08276c0ec5e8653315d4
LeeHW-Git-new <71817406+LeeHW-Git-new@users.noreply.github.com>
2021-08-03 18:09:26 +0900

    2021 08 03
    -기본 메인메뉴 생성

---
+++ Make-Amongus/Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EContorlType
+{
+    Mouse,
+    KeyboardMouse
+}
+
+public class PlayerSettings
+{
+    public static EContorlType controlType;
+}
